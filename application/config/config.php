1d7e155d09b4f599a573defa5ffb1a93136a91df
Demaspira <demaspiraa@gmail.com>
2018-09-17 09:44:04 +0700

    Change of base url

--- application/config/config.php
+++ application/config/config.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['base_url'] = 'http://localhost/surat/';
+$config['base_url'] = 'http://localhost/sps/';
 
 $config['index_page'] = '';
 
